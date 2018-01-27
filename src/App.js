44a057d3d99aeb5824809cb66225f2f40576ea62
Anton Lehmus <anton.lehmus@gmail.com>
2018-01-27 23:52:18 +0200

    separate trains into arriving and departing

--- src/App.js
+++ src/App.js
@@ -4,39 +4,41 @@ import _ from 'lodash';
 import './App.css';
 
 import SearchBar from './components/search_bar';
-import TraisTable from './components/trains_table';
+import TraisCard from './components/trains_card';
 
 const ROOT_URL = 'https://rata.digitraffic.fi/api/v1';
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props);
 
     this.state = {
       stations: [],
       selectedStation: {},
-      trains:[],
+      arriving_trains: [],
+      departing_trains: [],
       term: ''
     };
 
     this.fetchStations = this.fetchStations.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+    this.sortTrains = this.sortTrains.bind(this);
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.fetchStations();
   }
 
   //fetch passenger stations
-  fetchStations(){
+  fetchStations() {
     axios.get(`${ROOT_URL}/metadata/stations`)
       .then((response) => {
-        const passenger_stations = _.filter(response.data, function(o) { return o.passengerTraffic; });
+        const passenger_stations = _.filter(response.data, function (o) { return o.passengerTraffic; });
         this.setState({
-          stations:passenger_stations,
-          activeStation: {},
+          stations: passenger_stations,
+          selectedStation: {},
         });
       })
       .catch((error) => {
@@ -46,16 +48,49 @@ class App extends Component {
 
   fetchTrains(stationShortCode) {
     axios.get(`${ROOT_URL}/live-trains/station/${stationShortCode}?arrived_trains=0&arriving_trains=5&departed_trains=0&departing_trains=5`)
-    .then((response) => {
-      this.setState({
-        trains:response.data
+      .then((response) => {
+
+        const trains = response.data;
+        this.sortTrains(trains);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
   };
 
+  sortTrains(trains) {
+    let arriving_trains = [];
+    let departing_trains = [];
+
+    trains.forEach(t => {
+      let arrival_row = _.find(t.timeTableRows, { type: "ARRIVAL", stationShortCode: this.state.selectedStation.stationShortCode });
+      let departure_row = _.find(t.timeTableRows, { type: "DEPARTURE", stationShortCode: this.state.selectedStation.stationShortCode });
+
+      if(!_.isEmpty(arrival_row)){
+        arriving_trains.push({
+          'train': `${t.trainType} ${t.trainNumber}`,
+          'time': arrival_row.scheduledTime,
+          'from': t.timeTableRows[0].stationShortCode,
+          'to': t.timeTableRows[t.timeTableRows.length - 1].stationShortCode,
+          'platform': arrival_row.commercialTrack,
+          'cancelled': t.cancelled
+        });
+      }
+
+      if(!_.isEmpty(departure_row)){
+        departing_trains.push({
+          'train': `${t.trainType} ${t.trainNumber}`,
+          'time': departure_row.scheduledTime,
+          'from': t.timeTableRows[0].stationShortCode,
+          'to': t.timeTableRows[t.timeTableRows.length - 1].stationShortCode,
+          'platform': departure_row.commercialTrack,
+          'cancelled': t.cancelled
+        });
+      }
+    });
+
+    this.setState({ arriving_trains, departing_trains });
+  }
 
   handleSearchChange(term) {
     this.setState({ term });
@@ -63,17 +98,17 @@ class App extends Component {
 
   handleSearchSubmit() {
     //dirty,dirty bodge for Helsinkis double stations
-    if(_.isEqual(_.lowerCase(this.state.term),"helsinki") ) {
+    if (_.isEqual(_.lowerCase(this.state.term), "helsinki")) {
       this.setState({ term: 'Helsinki asema' });
     }
 
     //find user's station from stations array
     const station = _.find(this.state.stations,
-        (o) => { return _.isEqual(_.lowerCase(o.stationName), _.lowerCase(this.state.term)); } );
-        
-    if(station) {
-        this.setState({activeStation: station, term: ''});
-        this.fetchTrains(station.stationShortCode);
+      (o) => { return _.isEqual(_.lowerCase(o.stationName), _.lowerCase(this.state.term)); });
+
+    if (station) {
+      this.setState({ selectedStation: station, term: '' });
+      this.fetchTrains(station.stationShortCode);
     }
   }
 
@@ -81,8 +116,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar onSubmit={this.handleSearchSubmit} onChange={this.handleSearchChange} term={this.state.term}/>
-        <TraisTable activeStation={this.state.activeStation} trains={this.state.trains}/>
+        <SearchBar onSubmit={this.handleSearchSubmit} onChange={this.handleSearchChange} term={this.state.term} />
+        <TraisCard selectedStation={this.state.selectedStation} departing={this.state.departing_trains} arriving={this.state.arriving_trains} />
       </div>
     );
   }
---
+++ src/components/trains_card.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import TrainsTable from './trains_table';
+
+class TrainsCard extends Component {
+
+    render() {
+        if(_.isEmpty(this.props.selectedStation)){
+            return(
+                <div className="col-xs-12">
+                    <h4>Please select a station</h4>
+                </div>
+            );
+        }
+               
+        return(
+            <div className="card">
+                <div className="card-block">
+                    <h3 className="card-title">{ this.props.selectedStation.stationName }</h3>
+                    <div >
+                        <h4>Arriving</h4>
+                        <TrainsTable trains={this.props.arriving}/>
+                    </div>
+                    <div >
+                        <h4>Departig</h4>
+                        <TrainsTable trains={this.props.departing}/>
+                    </div>
+                </div>
+                
+            </div>
+        );
+    };
+
+}
+
+export default TrainsCard;
