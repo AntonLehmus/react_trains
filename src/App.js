af5d689967269b2fc87d9fe6c755a39ec912fade
Anton Lehmus <anton.lehmus@gmail.com>
2018-01-27 21:35:47 +0200

    add search_bar and stations and trains fetching

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import _ from 'lodash';
 import './App.css';
 
+import SearchBar from './components/search_bar';
+
+const ROOT_URL = 'https://rata.digitraffic.fi/api/v1';
+
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stations: [],
+      selectedStation: {},
+      trains:[],
+      term: ''
+    };
+
+    this.fetchStations = this.fetchStations.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  };
+
+  componentDidMount(){
+    this.fetchStations();
+  }
+
+  //fetch passenger stations
+  fetchStations(){
+    axios.get(`${ROOT_URL}/metadata/stations`)
+      .then((response) => {
+        const passenger_stations = _.filter(response.data, function(o) { return o.passengerTraffic; });
+        this.setState({
+          stations:passenger_stations,
+          selectedStation: {},
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  fetchTrains(stationShortCode) {
+    axios.get(`${ROOT_URL}/live-trains/station/${stationShortCode}?arrived_trains=0&arriving_trains=5&departed_trains=0&departing_trains=5`)
+    .then((response) => {
+      this.setState({
+        trains:response.data
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+
+  handleSearchChange(term) {
+    this.setState({ term });
+  }
+
+  handleSearchSubmit() {
+    //dirty,dirty bodge for Helsinkis double stations
+    if(_.isEqual(_.lowerCase(this.state.term),"helsinki") ) {
+      this.setState({ term: 'Helsinki asema' });
+    }
+
+    //find user's station from stations array
+    const station = _.find(this.state.stations,
+        (o) => { return _.isEqual(_.lowerCase(o.stationName), _.lowerCase(this.state.term)); } );
+        
+    if(station) {
+        this.setState({selectedStation: station, term: ''});
+        this.fetchTrains(station.stationShortCode);
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar onSubmit={this.handleSearchSubmit} onChange={this.handleSearchChange} term={this.state.term}/>
       </div>
     );
   }
---
+++ src/components/search_bar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+//import Autosuggest from 'react-autosuggest';
+
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onChange(e.target.value);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onSubmit();
+    }
+
+  render() {
+    const term = this.props.term;
+    
+
+    return (
+        <form className="form form-inline" onSubmit={this.handleSubmit}>
+            <div className="input-group col-sm-12">
+
+                <input type="text" className="form-control" id="search_bar"
+                placeholder="Search for station" value={term} onChange={this.handleChange} />
+
+                <span className="input-group-addon">
+                    <button className="btn btn-primary" type="submit" id="search_submit"><i className="fa fa-search "></i> Go!</button>
+                </span>
+            </div>
+      </form>
+    );
+  }
+
+}
+
+export default SearchBar;
